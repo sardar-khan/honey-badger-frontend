7f3bd108f8eb7a6060cd4f96af414670fa41eafc
Aun Shahbaz Awan <38175351+sardar-khan@users.noreply.github.com>
2022-05-09 18:40:28 +0500

    BrowserRouter to HashRouter

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap/dist/js/bootstrap.min.js';
 import TopNav from "./components/TopNav/TopNav";
 import Footer from "./components/Footer/Footer";
-import { BrowserRouter, Route, Routes } from 'react-router-dom';
+import { HashRouter, Route, Routes } from 'react-router-dom';
 import Home from "./pages/Home";
 import Claim from "./components/Claim/Claim";
 function App() {
@@ -12,14 +12,14 @@ function App() {
    <>
    {/* <Navbar/>
    <Form/> */}
-   <BrowserRouter>
+   <HashRouter>
    <TopNav/>
     <Routes>
       <Route path='/' element={<Home/>}/>
       <Route path='/claim-nft' element={<Claim/>}/>
     </Routes>
     <Footer/>
-   </BrowserRouter>
+   </HashRouter>
    </>
   );
 }
